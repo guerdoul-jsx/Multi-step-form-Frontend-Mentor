c4cfd2f431970f2f218daf342b861875bb5837b7
Mahmoud <mahmoudguedoul0@gmail.com>
2023-06-22 20:59:47 +0100

    remove unused functions

--- src/utils/Actions.ts
+++
@@ -1,7 +0,0 @@
-import { ChangeEvent } from "react";
-
-const handleKeyUpPhone = (event: ChangeEvent<HTMLInputElement>) => {
-  let inputValue = event.target.value;
-  const onlyNumber = inputValue.replace(/\D/g, "");
-  event.target.value = onlyNumber;
-};
