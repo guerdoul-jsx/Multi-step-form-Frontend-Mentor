6cf337143edc88bbec0ec6022ade95fdf4472935
Mahmoud <mahmoudguedoul0@gmail.com>
2023-06-22 20:32:24 +0100

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import "./App.css";
 import FormContainer from "./Components/FormContainer";
 import { StepsProvider } from "./Context/StepsContext";
 import { AppContainer } from "./main.style";
-import TestContainer from "./Components/TestContainer";
 
 function App() {
   return (
     <AppContainer>
       <StepsProvider>
         <Toaster />
-        {/* <TestContainer /> */}
         <FormContainer />
       </StepsProvider>
     </AppContainer>
